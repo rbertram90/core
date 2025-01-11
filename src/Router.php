c5fcebe3f7752241b6a5dc9531d37039327a5518
Ricky Bertram <ricky@rbwebdesigns.co.uk>
2025-01-11 01:44:37 +0000

    Start a better router

--- src/Router.php
+++ src/Router.php
@@ -17,4 +17,37 @@ class Router
         
         return false;
     }
+
+    /**
+     * Get the contents for a controller endpoint.
+     * 
+     * Route example:
+     * [
+     *    'controller' => MyController::class,
+     *    'function' => 'viewPage',
+     *    'methods' => ['GET'], // optional - restrict the request methods.
+     * ]
+     * 
+     * @todo parameters...
+     */
+    public function getContents(string $routeName) {
+        if (! $route = $this->lookup($routeName)) {
+            throw new \Exception("Unable to find route");
+        }
+
+        if (key_exists('methods', $route) && ! in_array($_SERVER['REQUEST_METHOD'], $route['methods'])) {
+            throw new \Exception("{$_SERVER['REQUEST_METHOD']} requests are not permitted on this route.");
+        }
+
+        if (! class_exists($route['controller'])) {
+            throw new \Exception("Unknown class: " . $route['controller']);
+        }
+
+        if (! method_exists($route['controller'], $route['function'])) {
+            throw new \Exception("Unknown method `{$route['function']}` on class `{$route['controller']}`");
+        }
+
+        $controller = new ($route['controller']);
+        return call_user_func([$controller, $route['function']]);
+    }
 }
